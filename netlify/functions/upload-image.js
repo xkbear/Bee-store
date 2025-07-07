929feca352a1fa2797ca6507dde6708a8345a36a
AshlynnLu <lkx.975@gmail.com>
2025-07-07 16:18:32 +0100

    imgbb

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -41,10 +41,17 @@ exports.handler = async function(event) {
   }
 
   try {
-    const response = await fetch(`https://api.imgbb.com/1/upload?key=${API_KEY}`, {
+    // 去除前缀 data:image/...;base64,
+    const cleanBase64 = image.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+
+    const params = new URLSearchParams();
+    params.append('key', API_KEY);
+    params.append('image', cleanBase64);
+
+    const response = await fetch('https://api.imgbb.com/1/upload', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ image })
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params.toString()
     });
 
     const data = await response.json();
